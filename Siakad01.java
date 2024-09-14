a4078045815886f72db89b75fdc23a0b815f5c34
Arsya-Fisa <Arsya Fikri S>
2024-09-14 22:22:24 +0700

    Tugas Daspro3

--- Siakad01.java
+++ Siakad01.java
@@ -5,33 +5,33 @@ public class Siakad01 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       
-      String nama, nim ;
-      char kelas;
-      byte absen;
-      double nilaiKuis, nilaiTugas, nilaiUjian, nilaiUAS, nilaiAkhir;
+      String nama, nim ; // variable tipe string
+      char kelas; // variable tipe char
+      byte absen; //variable tipe byte
+      double nilaiKuis, nilaiTugas, nilaiUjian, nilaiUAS, nilaiAkhir; // variable tipe double 
 
-      System.out.println("Masukan nama: ");
-      nama = sc.nextLine();
-      System.out.println("Masukan NIM: ");
-      nim = sc.nextLine();
-      System.out.println("Masukan kelas: ");
-      kelas = sc.nextLine(). charAt(0);
-      System.out.println("Masukan nomer absen: ");
-      absen = sc.nextByte();
+      System.out.println("Masukan nama: "); // menampilkan pesan untuk memasukan nama
+      nama = sc.nextLine(); // input nama
+      System.out.println("Masukan NIM: "); // menampilkan pesan untuk memasukan nim
+      nim = sc.nextLine(); // input nim
+      System.out.println("Masukan kelas: "); // menampilkan pesan untuk memasukan kelas 
+      kelas = sc.nextLine(). charAt(0); // membaca seluruh baris lalu mengambil nilai pertama
+      System.out.println("Masukan nomer absen: "); //menampilkan pesan absen
+      absen = sc.nextByte(); // input absen
 
-      System.out.println("Masukan nilai kuis= ");
-      nilaiKuis = sc.nextDouble();
-      System.out.println("Masukan nilai tugas= ");
-      nilaiTugas = sc.nextDouble();
-      System.out.println("Masukan nilai ujian= ");
-      nilaiUjian = sc.nextDouble();
-      System.out.println("Masukan nilai UAS= ");
-      nilaiUAS = sc.nextDouble();
+      System.out.println("Masukan nilai kuis= "); // input nilai kuis
+      nilaiKuis = sc.nextDouble(); // mengambil nilai input nilai kuis 
+      System.out.println("Masukan nilai tugas= "); // input nilai tugas
+      nilaiTugas = sc.nextDouble(); // mengambil nilai input nilai tugas 
+      System.out.println("Masukan nilai ujian= "); // input nilai ujian
+      nilaiUjian = sc.nextDouble(); // mengambil nilai input nilai ujian
+      System.out.println("Masukan nilai UAS= "); // input nilai UAS
+      nilaiUAS = sc.nextDouble(); // mengambil nilai input nilai UAS
 
       nilaiAkhir = (nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUjian * 0.30) + (nilaiUAS * 0.35) ;
 
-      System.out.println("Nama: " + nama + "NIM: " + nim);
-      System.out.println("Kelas: " + kelas + " Absen: " + absen);
-      System.out.println("Nilai Akhir: " + nilaiAkhir);
+      System.out.println("Nama: " + nama + "NIM: " + nim); // output nama dan Nim
+      System.out.println("Kelas: " + kelas + " Absen: " + absen); // output kelas dan absen
+      System.out.println("Nilai Akhir: " + nilaiAkhir); // output nilai akhir keseluruhan
     }
 }
